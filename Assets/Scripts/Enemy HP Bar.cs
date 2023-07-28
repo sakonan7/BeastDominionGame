91d33f5f339c0251d95cb51fe68421c04c0af4e6
Sammy7 <sakonan7@gmail.com>
2023-07-28 15:33:40 -0400

    Making New Target Appearance

--- Assets/Scripts/Enemy HP Bar.cs
+++ Assets/Scripts/Enemy HP Bar.cs
@@ -6,6 +6,7 @@ public class EnemyHPBar : MonoBehaviour
 {
     private GameObject cameraRef;
     private Quaternion lookRotation;
+    private Enemy enemyScript;
     // Start is called before the first frame update
     void Start()
     {
