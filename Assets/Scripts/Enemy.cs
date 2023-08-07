8ac91e8884b1378aaa832bc52e4f37badb16f688
Sammy7 <sakonan7@gmail.com>
2023-08-07 14:13:21 -0400

    Making Sure Attacks Only Land Once
    It worked, but I have to check the attack lag methods and the combo methods to ensure that they .

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -241,38 +241,42 @@ public class Enemy : MonoBehaviour
         //Debug.Log(HP + "Left");
         if (other.CompareTag("Tiger Attack Regular"))
         {
-            //For now, just trigger stun. I will use both of their directions to perform the knockback
-            //TakeDamage();
-            
-            HP -= 0;
-            HPBarScript.HPDecrease(2, originalHP);
-            //Damaged();
-            playerScript.PlayTigerRegularStrike(transform.position);
-            //Vector3 knockbackDirection = (transform.position - tiger.transform.position).normalized;
-            //knockback force is inconsistent. Sometimes it doesn't knockback at all. Sometimes it knocks back too much
-            //It doesn't matter what the value is.
-            //It may not matter because I will have the attack lag minimized
-            //But I don't want the player to whiff attacks, so I think I will make sure the tiger is the right distance from the wolf
-            //Unless I can make a force play until a certain distance is reached
-            //I can't use forcemode.impulse then
-            //Wow, I had to upgrade from 15 to 200 just to push foe back at most a few meters
-            //enemyRb.AddForce(playerScript.attackDirection * 160, ForceMode.Impulse);
-            //enemyRb.velocity = playerScript.attackDirection * 160;
-            //enemyRb.velocity = new Vector3(playerScript.attackDirection.x * 160, 0, playerScript.attackDirection.z * 160);
 
-            //Vector3 consistentVel = new Vector3(enemyRb.velocity.x, 0, enemyRb.velocity.z);
+            if (playerScript.attackLanded == false)
+            {
+                //For now, just trigger stun. I will use both of their directions to perform the knockback
+                //TakeDamage();
+
+                HP -= 2;
+                HPBarScript.HPDecrease(2, originalHP);
+                //Damaged();
+                playerScript.PlayTigerRegularStrike(transform.position);
+                //Vector3 knockbackDirection = (transform.position - tiger.transform.position).normalized;
+                //knockback force is inconsistent. Sometimes it doesn't knockback at all. Sometimes it knocks back too much
+                //It doesn't matter what the value is.
+                //It may not matter because I will have the attack lag minimized
+                //But I don't want the player to whiff attacks, so I think I will make sure the tiger is the right distance from the wolf
+                //Unless I can make a force play until a certain distance is reached
+                //I can't use forcemode.impulse then
+                //Wow, I had to upgrade from 15 to 200 just to push foe back at most a few meters
+                //enemyRb.AddForce(playerScript.attackDirection * 160, ForceMode.Impulse);
+                //enemyRb.velocity = playerScript.attackDirection * 160;
+                //enemyRb.velocity = new Vector3(playerScript.attackDirection.x * 160, 0, playerScript.attackDirection.z * 160);
+
+                //Vector3 consistentVel = new Vector3(enemyRb.velocity.x, 0, enemyRb.velocity.z);
 
 
-            //if (consistentVel.magnitude > 160)
-            //{
+                //if (consistentVel.magnitude > 160)
+                //{
                 //Vector3 limitedVel = consistentVel.normalized * 160;
                 //enemyRb.velocity = new Vector3(limitedVel.x, 0, limitedVel.z);
-            //}
-            
-            playerScript.AttackLandedTrue();
-            //Debug.Log(distance + " " + enemyRb.velocity);
-            StartCoroutine(FoeAttacked());
-            StartCoroutine(DamageDisplayDuration(0));
+                //}
+
+                playerScript.AttackLandedTrue();
+                //Debug.Log(distance + " " + enemyRb.velocity);
+                StartCoroutine(FoeAttacked());
+                StartCoroutine(DamageDisplayDuration(2));
+            }
         }
         if (other.CompareTag("Tiger Special"))
         {
